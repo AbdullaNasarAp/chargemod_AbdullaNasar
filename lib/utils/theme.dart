00899e6547c5d336e6005caa3590ffae5eaf7b15
AbdullaNasarAp <abdunasar000777@gmail.com>
2023-12-13 23:44:41 +0530

    dark and light theme done

--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -3,11 +3,9 @@ import 'package:abdulla_nasar/utils/reusable_widget.dart';
 import 'package:flutter/material.dart';
 
 final ThemeData lightTheme = ThemeData(
-  primaryColor: Colors.blue,
   scaffoldBackgroundColor: Colors.white,
   focusColor: HexColor("FFFFFF"),
   textTheme: TextTheme(
-    headlineLarge: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
     bodyLarge: textStyle(textColor: HexColor("2E2E2D")),
     bodySmall: textStyle(textColor: HexColor("666766")),
     bodyMedium: textStyle(textColor: HexColor("534B4A")),
@@ -15,11 +13,9 @@ final ThemeData lightTheme = ThemeData(
 );
 
 final ThemeData darkTheme = ThemeData.dark().copyWith(
-  primaryColor: Colors.indigo,
   focusColor: HexColor("FFFFFF"),
   scaffoldBackgroundColor: HexColor("2E2E2D"),
   textTheme: TextTheme(
-    headlineLarge: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
     bodyLarge: textStyle(textColor: HexColor("FFFFFF")),
     bodySmall: textStyle(textColor: HexColor("EBEBEB")),
     bodyMedium: textStyle(textColor: HexColor("534B4A")),
